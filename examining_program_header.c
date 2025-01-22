066cab3af8f2230a4c7a25a574f075beb0caa1eb
miaomingc <miaomingc@outlook.com>
2025-01-22 16:12:05 +0800

    exam_phdr

--- examining_program_header.c
+++ examining_program_header.c
@@ -6,11 +6,26 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+#define PRINT_FMT "   %-20s 0x%jx"
+#define PRINT_FIELD(N)                            \
+    do                                            \
+    {                                             \
+        printf(PRINT_FMT, #N, (uintmax_t)phdr.N); \
+    } while (0)
+#define NL()          \
+    do                \
+    {                 \
+        printf("\n"); \
+    } while (0)
+
 void print_ptype(size_t pt)
 {
     char *s;
-#define C(V) case PT_##V: s = #V;break
-    switch(pt)
+#define C(V)     \
+    case PT_##V: \
+        s = #V;  \
+        break
+    switch (pt)
     {
         C(NULL);
         C(LOAD);
@@ -22,36 +37,64 @@ void print_ptype(size_t pt)
         C(TLS);
         C(SUNWBSS);
         C(SUNWSTACK);
-//        C(SUNWDTRACE);
-//        C(SUNWCAP);
-        default :
-            s="unknown";
-            break;
+        //        C(SUNWDTRACE);
+        //        C(SUNWCAP);   PT_SUNWBSS and PT_SUNWSTACK not found in linux platform
+    default:
+        s = "unknown";
+        break;
     }
 #undef C
 }
-int main(int argc,char **argv)
+int main(int argc, char **argv)
 {
-    int i,fd;
+    int i, fd;
     Elf *e;
-    char *id,byte[5];
+    char *id, byte[5];
     size_t n;
     GElf_Phdr phdr;
-    if(argc!=2)
-        errx(EXIT_FAILURE,"usage : %s file-name",argv[0]);
-    if(elf_version(EV_CURRENT)==EV_NONE)
-        errx(EXIT_FAILURE,"ELF library initialization failed: %s",elf_errmsg(-1));
-    if((fd=open(argv[1],O_RDONLY,0))<0)
-        err(EXIT_FAILURE,"open %s failed",argv[1]);
-    if((e=elf_begin(fd,ELF_C_READ,NULL))==NULL)
-        errx(EXIT_FAILURE,"\"%s\" is not an ELF object",argv[1]);
-    if(elf_getphdrnum(e,&n)!=0)
-        errx(EXIT_FAILURE,"elf_getphdrnum() failed %s",elf_errmsg(-1));
-    for(i=0;i<n;i++)
+    if (argc != 2)
+        errx(EXIT_FAILURE, "usage : %s file-name", argv[0]);
+    if (elf_version(EV_CURRENT) == EV_NONE)
+        errx(EXIT_FAILURE, "ELF library initialization failed: %s", elf_errmsg(-1));
+    if ((fd = open(argv[1], O_RDONLY, 0)) < 0)
+        err(EXIT_FAILURE, "open %s failed", argv[1]);
+    if ((e = elf_begin(fd, ELF_C_READ, NULL)) == NULL)
+        errx(EXIT_FAILURE, "\"%s\" is not an ELF object", argv[1]);
+    if (elf_getphdrnum(e, &n) != 0)
+        errx(EXIT_FAILURE, "elf_getphdrnum() failed %s", elf_errmsg(-1));
+    for (i = 0; i < n; i++)
     {
-        if(gelf_getphdr(e,i,&phdr)!=&phdr)
-            errx(EXIT_FAILURE,"getphdr() failed : %s.",elf_errmsg(-1));
-        print("PHDR %d :\n",i);
+        if (gelf_getphdr(e, i, &phdr) != &phdr)
+            errx(EXIT_FAILURE, "getphdr() failed : %s.", elf_errmsg(-1));
+        printf("PHDR %d :\n", i);
+        PRINT_FIELD(p_type);
+        print_ptype(phdr.p_type);
+        NL();
+        PRINT_FIELD(p_offset);
+        NL();
+        PRINT_FIELD(p_vaddr);
+        NL();
+        PRINT_FIELD(p_paddr);
+        NL();
+        PRINT_FIELD(p_filesz);
+        NL();
+        PRINT_FIELD(p_memsz);
+        NL();
+        PRINT_FIELD(p_flags);
+        NL();
+        printf(" [ ");
+        if (phdr.p_flags & PF_X)
+            printf(" execute ");
+        if (phdr.p_flags & PF_R)
+            printf(" read ");
+        if (phdr.p_flags & PF_W)
+            printf(" write ");
+        printf(" ]");
+        NL();
+        PRINT_FIELD(p_align);
+        NL();
     }
+    elf_end(e);
+    close(fd);
     return EXIT_SUCCESS;
 }
